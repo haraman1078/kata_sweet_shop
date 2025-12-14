b250f7b670676b9ccc03eab88f4cc5432583941b
haraman1078 <harmandeeps1254@gmail.com>
2025-12-14 15:35:22 +0530

    chore: initial express + prisma setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const authRoutes = require('./routes/auth.routes');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Sweet Shop API is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const prisma = require('../prisma');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(201).json({
+      message: 'User registered successfully',
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+module.exports = router;
